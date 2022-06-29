2ffe5efb8271c6a090dede111a73f371f6228e81
chenxuetong <chenxuetong@noxgroup.com>
2022-06-29 16:51:59 +0800

    昨日阅读排行榜;

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import View
 from django.shortcuts import render_to_response
-from read_statistics.utils import get_seven_days_read_data,get_today_hot_data
+from read_statistics.utils import get_seven_days_read_data,get_today_hot_data,get_yesterday_hot_data
 from django.contrib.contenttypes.models import ContentType
 from blog.models import Blog
 class Home(View):
@@ -8,9 +8,10 @@ class Home(View):
     def get(self,request):
         blog_content_type = ContentType.objects.get_for_model(Blog)
         dates,read_nums = get_seven_days_read_data(blog_content_type)
-        today_hot_data = get_today_hot_data(blog_content_type)
+
         context = {}
         context['read_nums'] = read_nums
         context['dates'] = dates
-        context['today_hot_data'] = today_hot_data
+        context['today_hot_data'] = get_today_hot_data(blog_content_type)
+        context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
         return render_to_response(self.TEMPLATE,context)
--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -37,4 +37,10 @@ def get_seven_days_read_data(content_type):
 def get_today_hot_data(content_type):
     today = timezone.now().date()
     read_details = ReadDetail.objects.filter(content_type=content_type,date = today).order_by('-read_num')  #筛选完排序
-    return read_details
+    return read_details[:7]
+
+def get_yesterday_hot_data(content_type):
+    today = timezone.now().date()
+    yesterday = today - datetime.timedelta(days=1)
+    read_details = ReadDetail.objects.filter(content_type=content_type, date=yesterday).order_by('-read_num')  # 筛选完排序
+    return read_details[:7]
