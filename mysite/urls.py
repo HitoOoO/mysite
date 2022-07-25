05a83468bb4dc2a470645a0bb1d075798dbd5a02
chenxuetong <chenxuetong@noxgroup.com>
2022-07-25 16:25:57 +0800

    登录和注册功能实现;

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from blog import urls as blog_urls
 from comment import urls as comment_urls
-from mysite.views import Home,Login
+from mysite.views import Home,Login,Register
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -26,9 +26,11 @@ urlpatterns = [
     path('',Home.as_view(),name = 'home'),
     path('admin/', admin.site.urls),
     path('blog/',include(blog_urls)),
+    path('comment/',include(comment_urls)),
     path('ckeditor/',include('ckeditor_uploader.urls')),
     path('login/',Login.as_view(),name = 'login'),
-    path('comment/',include(comment_urls)),
+    path('register/',Register.as_view(),name = 'register'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -4,9 +4,10 @@ from read_statistics.utils import get_seven_days_read_data,get_today_hot_data,ge
 from django.contrib.contenttypes.models import ContentType
 from blog.models import Blog
 from django.core.cache import cache
-from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
 from django.urls import reverse
-
+from .forms import LoginForm, RegForm
+from django.contrib import auth
 class Home(View):
     TEMPLATE = 'home.html'
     def get(self,request):
@@ -27,16 +28,45 @@ class Home(View):
         return render(request,self.TEMPLATE,context)
 
 class Login(View):
-    TEMPLATE = 'error.html'
+    TEMPLATE = 'login.html'
+    def get(self,request):
+        login_form = LoginForm()
+        context = {}
+        context['login_form'] = login_form
+        return render(request,self.TEMPLATE,context)
     def post(self,request):
-        username = request.POST.get('username','')
-        password = request.POST.get('password','')
-        user = authenticate(username=username, password=password)
-        referer = request.META.get("HTTP_REFERER",reverse('home'))
-        if user is not None:
-            login(request,user)
-            return redirect(referer)
-        else:
-            return render(request,self.TEMPLATE,{'message':'用户名或者密码不正确'})
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = login_form.cleaned_data['user']
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
+        context = {}
+        context['login_form'] = login_form
+        return render(request, self.TEMPLATE, context)
+
+class Register(View):
+    TEMPLATE = 'register.html'
+    def get(self,request):
+        reg_form = RegForm()
+        context = {}
+        context['reg_form'] = reg_form
+        return render(request, 'register.html', context)
+    def post(self,request):
+        reg_form = RegForm(request.POST)
+        if reg_form.is_valid():
+            username = reg_form.cleaned_data['username']
+            email = reg_form.cleaned_data['email']
+            password = reg_form.cleaned_data['password']
+            # 创建用户
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            # 登录用户
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
+
+        context = {}
+        context['reg_form'] = reg_form
+        return render(request, self.TEMPLATE, context)
 
 
