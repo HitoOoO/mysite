cc43b4750e5b695c06bd34834b27ca8a0dbe9b0c
chenxuetong <chenxuetong@noxgroup.com>
2022-09-28 16:47:44 +0800

    使用邮箱登录;

--- user/forms.py
+++ user/forms.py
@@ -3,15 +3,21 @@ from django.contrib import auth
 from django.shortcuts import redirect,reverse
 from django.contrib.auth.models import User
 class LoginForm(forms.Form):
-    username = forms.CharField(label='用户名',widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'请输入用户名'}))
+    username_or_email = forms.CharField(label='用户名或邮箱',widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'请输入用户名或邮箱'}))
     password = forms.CharField(label='密码',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'请输入密码'}))
 
     def clean(self):
-        username = self.cleaned_data['username']
+        username_or_email = self.cleaned_data['username_or_email']
         password = self.cleaned_data['password']
 
-        user = auth.authenticate(username=username,password=password)
+        user = auth.authenticate(username=username_or_email,password=password)
         if user is None:
+            if User.objects.filter(email=username_or_email).exists():
+                username = User.objects.get(email=username_or_email).username
+                user = auth.authenticate(username=username, password=password)
+                if not user is None:
+                    self.cleaned_data['user'] = user
+                    return self.cleaned_data
             raise forms.ValidationError('用户名或者密码不正确')
         else:
             self.cleaned_data['user'] = user
